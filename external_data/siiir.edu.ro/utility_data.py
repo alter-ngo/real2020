0581bb1743cfe29abef2e01af68c2006b10c6d36
paubric <paubric@gmail.com>
2019-07-14 17:58:18 +0300

    Update utility_data.py

--- external_data/siiir.edu.ro/utility_data.py
+++ external_data/siiir.edu.ro/utility_data.py
@@ -2,17 +2,17 @@ import json
 import requests
 
 with open('data.json') as f:
-    siir_data = json.loads(f.read())
+    siiir_data = json.loads(f.read())
 
-print('Total:',len(siir_data))
+print('Total:', len(siiir_data))
 
-for index, hs in enumerate(siir_data):
-    hs_id = hs['ID_SCHOOL']
-    req = requests.get(f'https://www.siiir.edu.ro/carto/app/rest/school/buildingsstate/{hs_id}')
+for index, institution in enumerate(siiir_data):
+    institution_id = institution['ID_SCHOOL']
+    req = requests.get(
+        f'https://www.siiir.edu.ro/carto/app/rest/school/buildingsstate/{institution_id}')
     res = req.json()
-    siir_data[index]['OTHERS']['schoolBuildingUtilities'] = res['schoolBuildingUtilities']
-    print('Index:',index, '- Utilities:', len(res['schoolBuildingUtilities']))
+    siiir_data[index]['OTHERS']['schoolBuildingUtilities'] = res['schoolBuildingUtilities']
+    print('Index:', index, '- Utilities:', len(res['schoolBuildingUtilities']))
 
-with open('data.json','w') as f:
-    json.dump(siir_data,f)
-
+with open('data.json', 'w') as f:
+    json.dump(siiir_data, f)
