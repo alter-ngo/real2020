35786bb6eb355d756971d49a7376981b07827de9
paubric <paubric@gmail.com>
2019-07-14 16:10:54 +0300

    Closes #24

--- external_data/siiir.edu.ro/request_data.py
+++ external_data/siiir.edu.ro/request_data.py
@@ -5,24 +5,24 @@ import time
 with open('siiir_institutii.json') as f:
     data = json.loads(f.read())
     data = data['data']
-    institutii = data['content']
+    institutions = data['content']
 
 api_requests = ['details', 'subunits', 'organisation', 'materialresources']
 
 data = dict()
-for institutie_index in range(len(institutii)):
+for institution_index in range(len(institutions)):
     start_time = time.time()
-    print(institutie_index)
+    institution_data = dict()
 
-    institutie_data = dict()
     for req in api_requests:
         response = request.urlopen(
-            f'https://www.siiir.edu.ro/carto/app/rest/school/{req}/{institutii[institutie_index]["ID_SCHOOL"]}').read()
+            f'https://www.siiir.edu.ro/carto/app/rest/school/{req}/{institutions[institution_index]["ID_SCHOOL"]}').read()
         json_response = json.loads(response)
-        institutie_data[req] = json_response
-    institutii[institutie_index]['OTHERS'] = institutie_data
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
+        institution_data[req] = json_response
 
-json.dump(institutii, open('data.json', 'w',
-                           encoding='utf8'), ensure_ascii=False)
+    institutions[institution_index]['OTHERS'] = institution_data
+    elapsed_time = time.time() - start_time
+    print('[*]', institution_index, elapsed_time)
+
+json.dump(institutions, open('data.json', 'w',
+                             encoding='utf8'), ensure_ascii=False)
