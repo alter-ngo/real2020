d0bd21ee405d0c92e034c1e4cb659e565b5bf6c3
xfde <alex.constantin432@gmail.com>
2019-08-21 19:46:43 +0300

    Added color feedback on selection + fix #53

--- site/src/components/REAL/Objectives/index.js
+++ site/src/components/REAL/Objectives/index.js
@@ -6,20 +6,37 @@ class Objectives extends React.Component {
   constructor(props){
     super(props); 
     this.state={
-      selectionName:"Elevi"
+      selectionName:"Elevi",
+      colors:[
+      {id:1,value:"secondary-color",context:"Elevi"},
+      {id:2,value:"black",context:"Parinți"},
+      {id:3,value:"black",context:"Profesori"},
+      {id:4,value:"black",context:"Cercetători"},
+      {id:5,value:"black",context:"Conduceri"},
+      {id:6,value:"black",context:"Minister"},]
     };
+    
   }
-
   handleClick(mode){
+
+    for(let i=0;i<=5;i++)
+    {
+      if(this.state.colors[i].context==mode){
+        this.state.colors[i].value="#fa8c15"
+        console.log(this.state.colors[i].value,this.state.colors[i].context)
+      }
+      else{
+        this.state.colors[i].value="black"
+        console.log(this.state.colors[i].value,this.state.colors[i].context)
+      }
+    }    
     this.setState({
         selectionName: mode
     })
-
   }
+
   render(){
     return (
-
-        
           <Widget>
             <Row gutter={16} type="flex" align="middle">
             <Col xl={12} md={12} sm={12} xs={24}>
@@ -42,64 +59,64 @@ class Objectives extends React.Component {
             </Col>
               <Col xl={12} md={12} sm={12} xs={24}>
                 <Row gutter={8}>
-                  <Col span={8}>
-                    <Widget>
-                      <div onClick={this.handleClick.bind(this,"Elevi")}>
+                  <Col span={8} >
+                    <div onClick={this.handleClick.bind(this,"Elevi")}>
+                    <Widget style={{border:"20px solid black"}} >  
                       <img src= {image} alt= 'icon' height={60} width={60} />
                       <Divider/>
-                      <p style={{fontSize:"0.75em"}}>Elevi</p>
-                      </div>
+                      <p style={{fontSize:"0.75em", color:this.state.colors[0].value}}>Elevi</p>
                     </Widget>
+                    </div>
                   </Col>
                   <Col span={8}>
-                    <Widget>
-                      <div onClick={this.handleClick.bind(this,"Parinți")}>
+                    <div onClick={this.handleClick.bind(this,"Parinți")}>
+                    <Widget >
                       <img src= {image} alt= 'icon' height={60} width={60} />
                       <Divider/>
-                      <p style={{fontSize:"0.75em"}}>Parinți</p>
-                      </div>
+                      <p style={{fontSize:"0.75em", color:this.state.colors[1].value}}>Parinți</p>
                     </Widget>
+                    </div>
                   </Col>
                   <Col span={8}>
+                    <div onClick={this.handleClick.bind(this,"Profesori")}>
                     <Widget>
-                      <div onClick={this.handleClick.bind(this,"Profesori")}>
                       <img src= {image} alt= 'icon' height={60} width={60} />  
                       <Divider/>
-                      <p style={{fontSize:"0.75em"}}>Profesori</p>
-                      </div>
+                      <p style={{fontSize:"0.75em", color:this.state.colors[2].value}}>Profesori</p>
                     </Widget>
+                    </div>
                   </Col>
                 </Row>
                 <center>
                 <Row gutter={8}>
                   <Col span={8}>
+                    <div onClick={this.handleClick.bind(this,"Cercetători")}>
                 <Widget>
-                  <div onClick={this.handleClick.bind(this,"Cercetători")}>
                   <img src= {image} alt= 'icon' height={60} width={60} />
                   <Divider/>
                   <center>
-                  <p style={{fontSize:"0.75em"}}>Cercetători</p>
+                  <p style={{fontSize:"0.75em", color:this.state.colors[3].value}}>Cercetători</p>
                   </center>
-                  </div>
                 </Widget>
+                </div>
                   </Col>
                   <Col span={8}>
+                    <div onClick={this.handleClick.bind(this,"Conduceri")}>
                 <Widget>
-                  <div onClick={this.handleClick.bind(this,"Conduceri")}>
                   <img src= {image} alt= 'icon' height={60} width={60} />
                   <Divider/>
-                  <p style={{fontSize:"0.75em"}}>Conduceri</p>
-                  </div>
+                  <p style={{fontSize:"0.75em", color:this.state.colors[4].value}}>Conduceri</p>
                 </Widget>
+                </div>
                   </Col>
                   <Col span={8}>
+                    <div onClick={this.handleClick.bind(this,"Minister")}>
                 <Widget >
-                  <div onClick={this.handleClick.bind(this,"Minister")}>
                   <img src= {image} alt= 'icon' height={60} width={60} />
                   <Divider/>
-                  <p style={{fontSize:"0.75em"}}>Minister</p>
-                  </div>
+                  <p style={{fontSize:"0.75em", color:this.state.colors[5].value}}>Minister</p>
                 </Widget>
+                </div>
                   </Col>
                 </Row>
                 </center>
