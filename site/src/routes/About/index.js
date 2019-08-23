e7088a380cd5e667cc1fc233c054bf0b2bb44d81
xfde <alex.constantin432@gmail.com>
2019-08-23 09:15:05 +0300

    Added markdown for about page

--- site/src/routes/About/index.js
+++ site/src/routes/About/index.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { renderComponent } from "recompose";
+import textSource from "pages/despre.txt";
 
-import IntlMessages from "util/IntlMessages";
-import TeamMembers from "components/REAL/Team";
+class AboutPage extends React.Component {
+constructor(props){
+  super(props);
+  this.state={
+    text:'',
+    isLoading: false
+};
+}
+componentDidMount(){
+  this.setState({ isLoading: true });
+  fetch(textSource)
+  .then(response => response.text())
+  .then((data) => {
+      this.setState({text: data, isLoading: false});
+  })
+  .catch(error => this.setState({ error, isLoading: false }));
+}
 
-const AboutPage = () => {
-  return <TeamMembers />;
+render(){
+const ReactMarkdown = require('react-markdown/with-html')
+const TextDescription= this.state.text
+const isLoading= this.state.isLoading;
+
+  if (isLoading) {
+    return <p>Loading ...</p>;
+  }
+  return (
+
+  <ReactMarkdown
+  source={TextDescription}
+  escapeHtml={false}
+  />
+);
+}
 };
 
 export default AboutPage;
