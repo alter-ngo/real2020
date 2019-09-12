8cd9715b99b9a480c7b823ef63343059931f801a
paubric <paubric@gmail.com>
2019-09-12 19:49:48 +0200

    Update FormIntro

--- site/src/components/REAL/FormIntro/index.js
+++ site/src/components/REAL/FormIntro/index.js
@@ -9,7 +9,7 @@ const FormIntro = () => {
                 <Col>
                     <h1
                         className="gx-font-weight-semi-bold gs-fs-xlxl"
-                        style={{ fontSize: "2em" }}
+                        style={{ fontSize: "1.5em" }}
                     >
                         Bine ai venit în formularul #estereal!
                     </h1>
@@ -18,14 +18,17 @@ const FormIntro = () => {
                             <li>
                                 Opiniile exprimate de tine vor ajuta sute de mii de oameni, de la elevi care își aleg liceul și până la cercetători care realizează analize.
                             </li>
+                            <br/>
                             <li>
                                 Răspunsurile tale sunt <strong>100% anonime</strong>, fiind combinate cu răspunsurile a zeci de mii de alți elevi, profesori și tutori.
                             </li>
+                            <br/>
                             <li>
                                 Completarea formularului durează aproximativ 10 minute, acoperind subiecte diverse, de la oportunități până la organizare.
                             </li>
                         </ul>
                     </p>
+
                 </Col>
             </Row>
         </Widget>
