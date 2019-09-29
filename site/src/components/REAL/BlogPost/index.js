d942226f737891fa172bf32dca3336b56c87af6a
Sturza Mihai <sturzamihai23@gmail.com>
2019-09-29 14:34:37 +0300

    Handle redirects

--- site/src/components/REAL/BlogPost/index.js
+++ site/src/components/REAL/BlogPost/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Widget from "components/Widget";
 import { Row, Col, Button } from "antd";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import { string } from "prop-types";
 import BlogOverview from "../BlogOverview";
 
@@ -52,38 +52,48 @@ class BlogPost extends React.Component {
     const ReactMarkdown = require("react-markdown/with-html");
     return (
       <>
-      <Helmet>
-        <meta name="description" content={this.state.excerpt.substring(0,150)+" ..."}></meta>
-        <meta property="og:url" content={window.location.href}></meta>
-        <meta property="og:title" content={this.state.title}></meta>
-        <meta property="og:type" content="article"></meta>
-        <meta property="og:image" content={this.state.src}></meta>
-        <meta property="og:description" content={this.state.excerpt.substring(0,150)+" ..."}></meta>
-      </Helmet>
-      <Widget>
-        <Row gutter={16}>
-          <Col xl={18} md={16} sm={24} xs={24}>
-            <h1>{this.state.title}</h1>
-            <p>
-              {this.state.category} - {this.state.date}
-            </p>
-            <div className="content" style={{ color: "black", fontSize: "1.2em" }}>
-              <ReactMarkdown
-                source={this.state.content}
-                escapeHtml={false}
-              ></ReactMarkdown>
-            </div>
-          </Col>
-          <Col xl={6} md={8} sm={24} xs={24}>
-            <h2>Articole Recomandate</h2>
-            <BlogOverview
-              exclude={this.props.match.params.slug}
-              mode={"preview"}
-              embedded={true}
-            ></BlogOverview>
-          </Col>
-        </Row>
-      </Widget>
+        <Helmet>
+          <title>#estereal - {this.state.title}</title>
+          <meta
+            name="description"
+            content={this.state.excerpt.substring(0, 150) + " ..."}
+          ></meta>
+          <meta property="og:url" content={window.location.href}></meta>
+          <meta property="og:title" content={this.state.title}></meta>
+          <meta property="og:type" content="article"></meta>
+          <meta property="og:image" content={this.state.src}></meta>
+          <meta
+            property="og:description"
+            content={this.state.excerpt.substring(0, 150) + " ..."}
+          ></meta>
+        </Helmet>
+        <Widget>
+          <Row gutter={16}>
+            <Col xl={18} md={16} sm={24} xs={24}>
+              <h1>{this.state.title}</h1>
+              <p>
+                {this.state.category} - {this.state.date}
+              </p>
+              <div
+                className="content"
+                style={{ color: "black", fontSize: "1.2em" }}
+              >
+                <ReactMarkdown
+                  source={this.state.content}
+                  escapeHtml={false}
+                ></ReactMarkdown>
+              </div>
+            </Col>
+            <Col xl={6} md={8} sm={24} xs={24}>
+              <h2>Articole Recomandate</h2>
+              <BlogOverview
+                exclude={this.props.match.params.slug}
+                mode={"preview"}
+                embedded={true}
+              ></BlogOverview>
+            </Col>
+          </Row>
+        </Widget>
       </>
     );
   }
--- site/src/routes/Blog/index.js
+++ site/src/routes/Blog/index.js
@@ -1,21 +1,35 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-
+import { Helmet } from "react-helmet";
 import IntlMessages from "util/IntlMessages";
 import BlogOverview from "../../components/REAL/BlogOverview";
 import BlogPost from "../../components/REAL/BlogPost";
 
 const BlogPage = () => {
   return (
-    <div>
-      <h2 className="title gx-mb-4">
-        <IntlMessages id="Blog" />
-      </h2>
+    <>
+      <Helmet>
+        <title>#estereal - Blog</title>
+        <meta name="description" content="Postarile echipei REAL."></meta>
+        <meta property="og:url" content={window.location.href}></meta>
+        <meta property="og:title" content="Blog"></meta>
+        <meta property="og:type" content="article"></meta>
+        <meta property="og:image" content="loader.png"></meta>
+        <meta
+          property="og:description"
+          content="Postarile echipei REAL."
+        ></meta>
+      </Helmet>
+      <div>
+        <h2 className="title gx-mb-4">
+          <IntlMessages id="Blog" />
+        </h2>
 
-      <div className="gx-d-flex justify-content-center">
+        <div className="gx-d-flex justify-content-center">
           <BlogOverview></BlogOverview>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
